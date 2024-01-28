1df38c2a5be8f15af731581b916891e0445197a9
JuanLopez <mainhead@gmail.com>
2024-01-28 13:44:15 +0100

    a line to show stage area

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ProGitProgrammers
                 Console.log("Hello World");
                 Console.WriteLine("I just added this in Visual Studio Code");
                 Console.WriteLine("I just added this in the command line repo");
+                Console.WriteLine("This line added to show the staged area");
             }
         }
 }
