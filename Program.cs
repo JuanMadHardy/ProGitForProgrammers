6ad6b24d7889b73165503f48186d926e667ace0a
JuanLopez <mainhead@gmail.com>
2024-01-28 12:24:45 +0100

    crear Program -el primero-

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace ProGitProgrammers
+{
+        class Program 
+        {
+            static void Main([string[] args)
+            {
+                Console.log("Hello World");
+                Console.WriteLine("I just added this in Visual Studio Code");
+            }
+        }
+}
