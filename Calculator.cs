24870f433e8c8697c7b57d604c575f1e790ea7b8
JuanLopez <mainhead@gmail.com>
2024-01-29 12:30:59 +0100

    Add Substract method

--- Calculator.cs
+++ Calculator.cs
@@ -1,10 +1,17 @@
 namespace ProGitProgrammers
 {
     public class Calculator
-    {}
+    {
+
+    }
 
     public int Add (int left, int right)
     {
         return left + right;
+    }
+
+    public int Substract (int left, int right)
+    {
+        return left - right;
     }
 }
